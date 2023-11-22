c41c0429bbd63a2f5987c6556924af51f72921f5
abudowicklif <Abudowicklif@gmail.com>
2023-11-22 12:31:40 -0800

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,24 @@
-#!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
-  constructor (size) {
-    super(size, size);
-  }
-};
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
